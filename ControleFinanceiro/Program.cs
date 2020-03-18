3a18ff9c2b27cc4fea3babb1300fb579e787a304
Angelo Rafael Peres Pinto <angelorpt@gmail.com>
2020-03-18 17:52:05 -0400

    Initial Commit

---
+++ ControleFinanceiro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ControleFinanceiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
