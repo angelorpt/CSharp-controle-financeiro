e7d9bd27f19e3bfd1a56694d044587b5d748a59a
Angelo Rafael Peres Pinto <angelorpt@gmail.com>
2020-03-18 18:04:02 -0400

    Adicionado o método GetStringConnection da Classe Conexao.cs

--- Db/Conexao.cs
+++ Db/Conexao.cs
@@ -4,6 +4,11 @@ namespace Db
 {
     class Conexao
     {
-        
+        private static readonly string server   = "LAPTOP-B2ADTHUP\\SQLEXPRESS";
+        private static readonly string database = "SoN_Financeiro";
+        private static readonly string user     = "sa";
+        private static readonly string password = "root";
+
+        public static string GetStringConnection() => $"Server={server};Database={database};User Id={user};Password={password}";
     }
 }
