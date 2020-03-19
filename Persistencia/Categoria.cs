6078dea478a303485d1bb1f9e69b27a5b210b4d5
Angelo <angelorpt@gmail.com>
2020-03-19 11:50:23 -0400

    Implementação das classes de persistência: Categoria e ContaDAL.

--- Persistencia/Categoria.cs
+++ Persistencia/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.SqlClient;
+using ModelCategoria = Modelo.Categoria;
 
 namespace Persistencia
 {
@@ -10,6 +11,28 @@ namespace Persistencia
         public Categoria (SqlConnection conn)
         {
             this.conn = conn;
+        }
+
+        public ModelCategoria GetCategoria(int id)
+        {
+            ModelCategoria categoria = new ModelCategoria();
+
+            string SQL;
+            SQL = "SELECT ID, NOME FROM CATEGORIA WHERE ID = @id";
+            var cmd = new SqlCommand(SQL, this.conn);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            this.conn.Open();
+
+            using (SqlDataReader dr = cmd.ExecuteReader())
+            {
+                dr.Read();
+                categoria.Id   = Convert.ToInt32(dr["id"].ToString());
+                categoria.Nome = dr["nome"].ToString();
+            }
+
+            this.conn.Close();
+            return categoria;
         }
     }
 }
--- Persistencia/ContaDAL.cs
+++ Persistencia/ContaDAL.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Text;
+using Modelo;
 
 namespace Persistencia
 {
@@ -14,6 +15,43 @@ namespace Persistencia
         {
             this.conn = conn;
             string strConn = Db.Conexao.GetStringConnection();
+            this.categoria = new Categoria(new SqlConnection(strConn));
         }
+
+        public List<Conta> ListarTodos()
+        {
+            List<Conta> contas = new List<Conta>();
+
+            string SQL;
+            SQL  = "SELECT CON.ID, CON.DESCRICAO, CON.VALOR, CON.TIPO, CON.DATA_VENCIMENTO, CAT.NOME, CAT.ID AS CATEGORIA_ID ";
+            SQL += "FROM CONTAS CON";
+            SQL += "INNER JOIN CATEGORIAS CAT ON CON.CATEGORIA_ID = CAT.ID; ";
+
+            var cmd = new SqlCommand(SQL, this.conn);
+            
+            this.conn.Open();
+
+            using (SqlDataReader dr = cmd.ExecuteReader())
+            {
+                while(dr.Read())
+                {
+                    Conta conta = new Conta()
+                    {
+                        Id        = Convert.ToInt32(dr["id"].ToString()),
+                        Descricao = dr["id"].ToString(),
+                        Tipo      = Convert.ToChar(dr["id"].ToString()),
+                        Valor     = Convert.ToDouble(dr["id"].ToString())
+                    };
+
+                    int id_categoria = Convert.ToInt32(dr["id"].ToString());
+                    conta.categoria  = this.categoria.GetCategoria(id_categoria);
+                    contas.Add(conta);
+                }
+            }
+
+            return contas;
+
+        }
+
     }
 }
