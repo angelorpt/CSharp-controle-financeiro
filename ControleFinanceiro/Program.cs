5a061e4fe6c011d46d5ff13ef407e8aaa2935cb5
Angelo Rafael Peres Pinto <angelorpt@gmail.com>
2020-03-19 16:20:31 -0400

    Adicionada a funcionalidade para listar as contas

--- ControleFinanceiro/Program.cs
+++ ControleFinanceiro/Program.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using static System.Console;
+using System.Collections.Generic;
+
+using Modelo;
+using Persistencia;
+using System.Data.SqlClient;
+
+using ConsoleTables;
 
 namespace ControleFinanceiro
 {
     class Program
     {
+
+        private List<Conta> contas;
+        private List<Categoria> categorias;
+
+        private ContaDAL conta;
+        private CategoriaDAL categoria;
+
+        public Program()
+        {
+            string strConn = Db.Conexao.GetStringConnection();
+
+            this.conta     = new ContaDAL(new SqlConnection(strConn));
+            this.categoria = new CategoriaDAL(new SqlConnection(strConn));
+        }
+
         static void Main(string[] args)
         {
 
             int opc;
+
+            Program p = new Program();
 
             do
             {
@@ -30,7 +54,22 @@ namespace ControleFinanceiro
                     switch(opc)
                     {
                         case 1:
-                            Write("Listar");
+
+                            Title = "LISTAGEM DE CONTAS - CONTROLE FINANCEIRO";
+                            Uteis.MontaHeader("LISTAGEM DE CONTAS");
+
+                            p.contas = p.conta.ListarTodos();
+
+                            ConsoleTable table = new ConsoleTable("ID", "Descrição", "Tipo", "Valor");
+
+                            foreach (var conta in p.contas)
+                            {
+                                table.AddRow(conta.Id, conta.Descricao, conta.Tipo.Equals('R') ? "Receber" : "Pagar", string.Format("{0:c}", conta.Valor));
+                            }
+                            table.Write();
+                            ReadLine();
+                            Clear();
+
                             break;
 
                         case 2:
@@ -53,7 +92,6 @@ namespace ControleFinanceiro
 
             } while (opc != 6);
 
-            ReadLine();
         }
     }
 }
