8e425a5246b01e0d4c50152829c98d8e1b66a868
Angelo Rafael Peres Pinto <angelorpt2@gmail.com>
2020-03-19 16:20:31 -0400

    Adicionada a funcionalidade para listar as contas

---
+++ Db/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Db
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+
+        }
+    }
+}
