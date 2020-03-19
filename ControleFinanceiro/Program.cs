5b961c3a70a67172a17ad11e3f15aad4b2146fc5
Angelo Rafael Peres Pinto <angelorpt@gmail.com>
2020-03-19 09:48:23 -0400

    Adicionado o Menu

--- ControleFinanceiro/Program.cs
+++ ControleFinanceiro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace ControleFinanceiro
 {
@@ -6,7 +7,53 @@ namespace ControleFinanceiro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! 111111111");
+
+            int opc;
+
+            do
+            {
+                Title = "Controle Financeiro";
+                Uteis.MontaMenu();
+
+                opc = Convert.ToInt32(ReadLine());
+
+                if ( !(opc >= 1 && opc <= 6) )
+                {
+                    Clear();
+                    BackgroundColor = ConsoleColor.Red;
+                    ForegroundColor = ConsoleColor.White;
+                    Uteis.MontaHeader("INFORME OPÇÃO VÁLIDA!", 'X', 40);
+                    ResetColor();
+                } else
+                {
+                    Clear();
+                    switch(opc)
+                    {
+                        case 1:
+                            Write("Listar");
+                            break;
+
+                        case 2:
+                            Write("Cadastrar");
+                            break;
+
+                        case 3:
+                            Write("Editar");
+                            break;
+
+                        case 4:
+                            Write("Excluir");
+                            break;
+
+                        case 5:
+                            Write("Relatório");
+                            break;
+                    }
+                }
+
+            } while (opc != 6);
+
+            ReadLine();
         }
     }
 }
